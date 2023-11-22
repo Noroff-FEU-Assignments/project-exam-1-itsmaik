9a6a31ce4ce6c589a1c2f69bfcc1b812342807d3
Maik Cavalcante Dos Santos <126066270+itsmaik@users.noreply.github.com>
2023-11-22 21:27:38 +0100

    fetching data to the front page
    Fashion and Editorial section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 import Header from "./Header.js"
 
-// IDs or slugs of my specific posts
-const postIdentifiers = ['fw23-lookbook-carhartt', 'hinterland-lookbook-yoke', 'q23-lookbook-butter-goods'];
 
-// Fetching and displaying each post
-postIdentifiers.forEach((postIdentifier, index) => {
-  fetch(`https://meninfashion.itsmaik.com/wp-json/wp/v2/posts?_embed&slug=${postIdentifier}`)
+function fetchAndDisplayPostsByCategory(categoryId, elementIdPrefix, basePath, order = 'desc', orderby = 'date') {
+  fetch(`https://meninfashion.itsmaik.com/wp-json/wp/v2/posts?_embed&categories=${categoryId}&order=${order}&orderby=${orderby}`)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -13,28 +10,22 @@ postIdentifiers.forEach((postIdentifier, index) => {
       return response.json();
     })
     .then(posts => {
-      
-      const post = posts[0];
+      posts.forEach((post, index) => {
 
-      const imgElement = document.getElementById(`editorial-img-${index + 1}`);
-      const anchorElement = document.getElementById(`editorial-link-${index + 1}`);
+        const imgElement = document.getElementById(`${elementIdPrefix}-img-${index + 1}`);
+        const anchorElement = document.getElementById(`${elementIdPrefix}-link-${index + 1}`);
 
-      if (imgElement && post._embedded['wp:featuredmedia']) {
-        imgElement.src = post._embedded['wp:featuredmedia'][0].source_url;
-      }
+        if (imgElement && post._embedded['wp:featuredmedia']) {
+          imgElement.src = post._embedded['wp:featuredmedia'][0].source_url;
+        }
 
-      if (anchorElement) {
-        anchorElement.href = `./pages/editorial.html?slug=${post.slug}`;
-        anchorElement.addEventListener('click', () => {
-         
-          localStorage.setItem('currentPostSlug', post.slug);
-        });
-      }
+        if (anchorElement) {
+          anchorElement.href = `${basePath}?slug=${post.slug}`;
+        }})
     })
     .catch(error => console.error('Fetch error:', error));
-});
+}
 
+fetchAndDisplayPostsByCategory('13', 'editorial', '../pages/editorial.html', 'asc', 'date');
 
-// fetch 4 porst styles 
-
-// fetch fashion pots   
+fetchAndDisplayPostsByCategory('10', 'fashion', '../pages/fashion-posts.html', 'asc', 'date');
