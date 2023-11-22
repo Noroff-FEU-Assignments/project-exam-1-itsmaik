838bef778ac975fc5862e2f3e7306d3f6c7c8268
Maik Cavalcante Dos Santos <126066270+itsmaik@users.noreply.github.com>
2023-11-22 22:39:59 +0100

    Update front page data fetching

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import Header from "./Header.js"
 
 
-function fetchAndDisplayPostsByCategory(categoryId, elementIdPrefix, basePath, order = 'desc', orderby = 'date') {
+function fetchAndDisplayPostsByCategory(categoryId, structureType, order, orderby, elementIdPrefix, basePath) {
   fetch(`https://meninfashion.itsmaik.com/wp-json/wp/v2/posts?_embed&categories=${categoryId}&order=${order}&orderby=${orderby}`)
     .then(response => {
       if (!response.ok) {
@@ -12,20 +12,59 @@ function fetchAndDisplayPostsByCategory(categoryId, elementIdPrefix, basePath, o
     .then(posts => {
       posts.forEach((post, index) => {
 
-        const imgElement = document.getElementById(`${elementIdPrefix}-img-${index + 1}`);
-        const anchorElement = document.getElementById(`${elementIdPrefix}-link-${index + 1}`);
+        switch(structureType) {
+          case 'editorial':
+            displayEditorialPost(post, index, elementIdPrefix, basePath);
+            break;
+          case 'fashion':
+            displayFashionPost(post, index, elementIdPrefix, basePath);
+            break;
+          case 'trending':
+            displayTrendingPost(post, index, elementIdPrefix, basePath);
+            break;
+          case 'carousel':
+            displayCarouselPost(post, index, elementIdPrefix, basePath);
+            break;
 
-        if (imgElement && post._embedded['wp:featuredmedia']) {
-          imgElement.src = post._embedded['wp:featuredmedia'][0].source_url;
-        }
-
-        if (anchorElement) {
-          anchorElement.href = `${basePath}?slug=${post.slug}`;
-        }})
-    })
+          default:
+            console.error('Unknown structure type');
+          }
+    });
+  })
     .catch(error => console.error('Fetch error:', error));
 }
 
-fetchAndDisplayPostsByCategory('13', 'editorial', '../pages/editorial.html', 'asc', 'date');
+function displayEditorialPost(post, index, elementIdPrefix, basePath) {
+  const imgElement = document.getElementById(`${elementIdPrefix}-img-${index + 1}`);
+  const anchorElement = document.getElementById(`${elementIdPrefix}-link-${index + 1}`);
 
-fetchAndDisplayPostsByCategory('10', 'fashion', '../pages/fashion-posts.html', 'asc', 'date');
+  if (imgElement && post._embedded['wp:featuredmedia']) {
+    imgElement.src = post._embedded['wp:featuredmedia'][0].source_url;
+  }
+
+  if (anchorElement) {
+    anchorElement.href = `${basePath}?slug=${post.slug}`;
+  }
+}
+
+function displayFashionPost(post, index, elementIdPrefix, basePath) {
+  const imgElement = document.getElementById(`${elementIdPrefix}-img-${index + 1}`);
+  const anchorElement = document.getElementById(`${elementIdPrefix}-link-${index + 1}`);
+
+  if (imgElement && post._embedded['wp:featuredmedia']) {
+    imgElement.src = post._embedded['wp:featuredmedia'][0].source_url;
+  }
+
+  if (anchorElement) {
+    anchorElement.href = `${basePath}?slug=${post.slug}`;
+  }
+}
+
+const editorialElementIdPrefix = 'editorial';
+const editorialBasePath = '../pages/editorial.html';
+const fashionElementIdPrefix = 'fashion';
+const fashionBasePath = '../pages/fashion-posts.html';
+
+fetchAndDisplayPostsByCategory('13', 'editorial', 'asc', 'date', editorialElementIdPrefix, editorialBasePath);
+
+fetchAndDisplayPostsByCategory('10', 'fashion', 'asc', 'date', fashionElementIdPrefix, fashionBasePath );
