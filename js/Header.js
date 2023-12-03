a3ab161801e5c257eec7cf56a98a6688e2a87c88
Maik Cavalcante Dos Santos <126066270+itsmaik@users.noreply.github.com>
2023-12-03 15:46:11 +0100

    Hamburger menu

--- js/Header.js
+++ js/Header.js
@@ -7,7 +7,7 @@ class Header {
   }
 
   generateHeader() {
-    const heroImage = document.querySelector("#hero-image")
+    const heroImage = document.querySelector("#hero-image");
     const navItemsHTML = this.navItems.map(item => `<li><a href="${item.link}">${item.text}</a></li>`).join('');
     document.querySelector("#nav-bar").innerHTML = `<nav><ul>${navItemsHTML}</ul></nav>`;
 
@@ -15,7 +15,34 @@ class Header {
       heroImage.src = this.heroImagePath;
     }
 
-    document.querySelector("#logo").innerHTML = `<h1>${this.logoText}<h1>`;
+    document.querySelector("#logo").innerHTML = `<h1>${this.logoText}</h1>`;
+    // Add hamburger icon and close icon
+    const menuIcon = document.createElement('div');
+    menuIcon.id = 'menu-icon';
+    menuIcon.innerHTML = '&#9776;';
+    
+    const closeIcon = document.createElement('div');
+    closeIcon.id = 'close-icon';
+    closeIcon.innerHTML = 'x';
+
+    const header = document.querySelector("header");
+    const navBar = document.querySelector("#nav-bar");
+    header.appendChild(menuIcon);
+    header.insertBefore(closeIcon, navBar);
+
+    menuIcon.addEventListener("click", () => {
+      const navBar = document.querySelector("#nav-bar");
+      navBar.classList.toggle("active");
+      menuIcon.style.display = 'none';
+      closeIcon.style.display = 'flex';
+    });
+
+    closeIcon.addEventListener("click", () => {
+      const navBar = document.querySelector("#nav-bar");
+      navBar.classList.remove("active");
+      closeIcon.style.display = 'none';
+      menuIcon.style.display = 'flex';
+    });
   }
 }
 
